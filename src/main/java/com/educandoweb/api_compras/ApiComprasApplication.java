b7c94bee80717a3b2049729c0df1e27c0402c2d8
vazvdr <azevedo.vanderson@hotmail.com>
2024-07-13 17:25:06 -0300

    Criando o projeto

---
+++ src/main/java/com/educandoweb/api_compras/ApiComprasApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.api_compras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiComprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiComprasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educandoweb/api_compras/ApiComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.api_compras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiComprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
