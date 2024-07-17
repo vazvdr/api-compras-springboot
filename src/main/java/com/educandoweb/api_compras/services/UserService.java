5fab7e16eb644ad5bdb635fc23d5772447459672
vazvdr <azevedo.vanderson@hotmail.com>
2024-07-17 12:01:33 -0300

    Enviando o tratamento de excessão do update user

--- src/main/java/com/educandoweb/api_compras/services/UserService.java
+++ src/main/java/com/educandoweb/api_compras/services/UserService.java
@@ -13,6 +13,8 @@ import com.educandoweb.api_compras.repositories.UserRepository;
 import com.educandoweb.api_compras.services.exceptions.DatabaseException;
 import com.educandoweb.api_compras.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -43,10 +45,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj){
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
-		
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
