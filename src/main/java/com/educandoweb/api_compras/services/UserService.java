0fad86312d577a43023b7352a929d93c593b3706
vazvdr <azevedo.vanderson@hotmail.com>
2024-07-17 11:42:47 -0300

    Enviando o tratamento de excessão do delete user

--- src/main/java/com/educandoweb/api_compras/services/UserService.java
+++ src/main/java/com/educandoweb/api_compras/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.api_compras.entities.User;
 import com.educandoweb.api_compras.repositories.UserRepository;
+import com.educandoweb.api_compras.services.exceptions.DatabaseException;
 import com.educandoweb.api_compras.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -20,9 +23,9 @@ public class UserService {
 		return repository.findAll();
 	}
 	
-	public User findById(Long Id) {
-		Optional<User> obj = repository.findById(Id);
-		return obj.orElseThrow(() -> new ResourceNotFoundException(Id));	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));	
 	}
 	
 	public User insert(User obj) {
@@ -30,7 +33,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj){
